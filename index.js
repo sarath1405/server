354c757f782fdef2c2cc63f328e6aef55be8f64a
sarath1405 <sarathchandra1426@gmail.com>
2022-05-20 13:52:52 +0530

    initial

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ app.use(cors({
     credentials : true
 }))
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
