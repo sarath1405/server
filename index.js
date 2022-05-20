00e5bf85551cd5c460401a1cc2d0df7405a6f89e
sarath1405 <sarathchandra1426@gmail.com>
2022-05-20 11:54:25 +0530

    initial

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express();
-
-// app.use(cors({
-//     origin : '*',
-//     credentials : true,
-// }));
-app.options('*', cors())
 app.use(express.json());
+
+app.use(cors({
+    origin : '*',
+    credentials : true,
+}));
 
 mongoose.connect("mongodb+srv://sarath14:Gd0b6Vc5Awqkw1ev@cluster0.arzcw.mongodb.net/BlogApp?retryWrites=true&w=majority").then(console.log('mongodb connected!'))
 .catch("error");
@@ -18,6 +17,6 @@ mongoose.connect("mongodb+srv://sarath14:Gd0b6Vc5Awqkw1ev@cluster0.arzcw.mongodb
 app.use('/', require('./routes/auth'))
 app.use('/', require('./routes/blog'))
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('5000'));
+app.listen(PORT, () => console.log('5000'));
