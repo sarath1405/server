3119b41a4a252c22a89f61cf3fbf5c62c40c201d
sarath1405 <sarathchandra1426@gmail.com>
2022-05-20 14:02:07 +0530

    initial

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ require('dotenv').config()
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin : '*',
-    credentials : true
-}))
+// app.use(cors({
+//     origin : '*',
+//     credentials : true
+// }))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 const PORT = process.env.PORT || 5000;
 
