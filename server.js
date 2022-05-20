57dffa31b36c3e7e4d105b7f4edfc01cac93616c
sarath1405 <sarathchandra1426@gmail.com>
2022-05-20 10:36:58 +0530

    commit2

--- server.js
+++ server.js
@@ -4,19 +4,22 @@ const mongoose = require('mongoose')
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin : '*'
+}));
 app.use(express.json());
 
 mongoose.connect("mongodb+srv://sarath14:Gd0b6Vc5Awqkw1ev@cluster0.arzcw.mongodb.net/BlogApp?retryWrites=true&w=majority").then(console.log('mongodb connected!'))
 .catch("error");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", 'X-Requested-With,Content-Type');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.header("Access-Control-Allow-Headers", 'X-Requested-With,Content-Type');
+//     next();
+// });
+
 
 app.use('/', require('./routes/auth'))
 app.use('/', require('./routes/blog'))
