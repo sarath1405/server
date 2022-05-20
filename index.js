cc390858f2ea1c144d455846f9415cb7e96d0f4a
sarath1405 <sarathchandra1426@gmail.com>
2022-05-20 12:07:54 +0530

    initial

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(cors({
     origin : '*',
-    credentials : true,
-}));
-
-mongoose.connect("mongodb+srv://sarath14:Gd0b6Vc5Awqkw1ev@cluster0.arzcw.mongodb.net/BlogApp?retryWrites=true&w=majority").then(console.log('mongodb connected!'))
-.catch("error");
-
-
-app.use('/', require('./routes/auth'))
-app.use('/', require('./routes/blog'))
+    credentials : true
+}))
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('5000'));
+app.listen(PORT, () => {
+    console.log("server is running on PORT", PORT);
+})
+
+mongoose.connect("mongodb+srv://sarath14:Gd0b6Vc5Awqkw1ev@cluster0.arzcw.mongodb.net/BlogApp?retryWrites=true&w=majority").then(console.log('mongodb connected!'))
+.catch("error");
