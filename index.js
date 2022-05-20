4974b1c22300b15093c7f5633ddc309cc0a53964
sarath1405 <sarathchandra1426@gmail.com>
2022-05-20 12:10:22 +0530

    initial

--- index.js
+++ index.js
@@ -16,5 +16,8 @@ app.listen(PORT, () => {
     console.log("server is running on PORT", PORT);
 })
 
+app.use('/', require('./routes/auth'))
+app.use('/', require('./routes/blog'))
+
 mongoose.connect("mongodb+srv://sarath14:Gd0b6Vc5Awqkw1ev@cluster0.arzcw.mongodb.net/BlogApp?retryWrites=true&w=majority").then(console.log('mongodb connected!'))
 .catch("error");
